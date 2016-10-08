9bb7c8742afa496abded0ca4ba4e4072d3ad827e
toddsby <toddsby@gmail.com>
2016-10-08 16:40:10 -0500

    fixed syntax error (space after return statement) preventing seeder from running

--- db/seeds/20161007171611-add-initial-users.js
+++ db/seeds/20161007171611-add-initial-users.js
@@ -3,18 +3,16 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     console.log('GO SPEEDRACER GOOO!');
-    return 
-      queryInterface.bulkInsert('users', [
-        {email: "aphrodite@olympus.org", name: "Aphrodite", passwordDigest: '', createdAt: Date.now(), updatedAt: Date.now() },
-        {email: "athena@olympus.org", name: "Athena", passwordDigest: '', createdAt: Date.now(), updatedAt: Date.now() },
-        {email: "zeus@olympus.org", name: "Zeus", passwordDigest: '', createdAt: Date.now(), updatedAt: Date.now() },
-        {email: "apollo@olympus.org", name: "Apollo", passwordDigest: '', createdAt: Date.now(), updatedAt: Date.now() }
+    return queryInterface.bulkInsert('users', [
+        {email: "aphrodite@olympus.org", name: "Aphrodite", created_at: Date.now(), updated_at: Date.now() },
+        {email: "athena@olympus.org", name: "Athena", created_at: Date.now(), updated_at: Date.now() },
+        {email: "zeus@olympus.org", name: "Zeus", created_at: Date.now(), updated_at: Date.now() },
+        {email: "apollo@olympus.org", name: "Apollo", created_at: Date.now(), updated_at: Date.now() }
         ],{}
       );
   },
 
   down: function (queryInterface, Sequelize) {
-    return 
-    queryInterface.bulkDelete({tablename:'users'}, null, {});
+    return queryInterface.bulkDelete({tablename:'users'}, null, {});
   }
 };
